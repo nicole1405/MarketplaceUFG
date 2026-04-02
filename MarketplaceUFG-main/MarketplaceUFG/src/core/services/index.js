ea7e9310f35c6a44200459ffa46e7a4827f6de20
vekzz-dev <194902316+vekzz-dev@users.noreply.github.com>
2026-04-01 23:28:51 -0600

    Revert "Merge branch 'master'"
    This reverts commit 38278decf6d8a33ff1b6d103f1f543e13871e519, reversing
    changes made to 8f46e025ab459f42bf10b0149296c273e383aeba.

--- MarketplaceUFG-main/MarketplaceUFG/src/core/services/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Exportaciones de servicios
- */
-
-export { AuthService } from './AuthService.js';
-export { ProductService } from './ProductService.js';
-export { ChatService } from './ChatService.js';
-export { CategoryService } from './CategoryService.js';
--- MarketplaceUFG-main/MarketplaceUFG/src/core/utils/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Exportaciones de utilidades
- */
-
-export { EventEmitter, eventBus } from './EventEmitter.js';
-export { ImageUtils } from './ImageUtils.js';
-export { Validator } from './Validator.js';
-export { UIUtils } from './UIUtils.js';
-export { ToastService, toast } from './ToastService.js';
